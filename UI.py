8c47d308a9902b0a1feb706a7d8ca998ecfdeb76
Amberimzyj <amberimzyj@outlook.com>
2018-04-23 17:23:52 +0800

    Initial commit

---
+++ UI.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: Amberimzyj
+# @Emial:  amberimzyj@outlook.com
+# @Date:   2018-04-23 17:18:49
+# @Last Modified by:   Amberimzyj
+# @Last Modified time: 2018-04-23 17:21:48
+# @License: MIT LICENSE
+from tkinter import *
